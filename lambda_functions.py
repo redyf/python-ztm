a5a900454b7c8db3bf0375beefc8aaad259d7046
Redyf <mateusalvespereira7@gmail.com>
2023-11-14 18:49:59 -0300

    learning new stuff

---
+++ lambda_functions.py
@@ -0,0 +1,11 @@
+# Lambda Expressions
+from functools import reduce
+
+my_list = [1, 2, 3]
+# lambda param: action(param)
+
+
+print(list(map(lambda item: item * 2, my_list)))
+print(list(filter(lambda item: item % 2 != 0, my_list)))
+print(reduce(lambda acc, item: acc + item, my_list))
+print(my_list)
